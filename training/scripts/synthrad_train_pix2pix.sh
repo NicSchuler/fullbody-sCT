5c5aaa4bec2223416c08ee3bd794c4e42cd8b8bf
Flavian Thuer <flavian.thuer@hotmail.com>
2025-11-16 17:04:02 +0100

    add training folder

---
+++ training/scripts/synthrad_train_pix2pix.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 python train.py --dataroot /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/6materialized_splitsNonNormalized/pix2pix/AB --name pix2pix_synthrad_whole_data --model pix2pix --direction AtoB --input_nc 1 --output_nc 1
