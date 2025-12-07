ab3f4071c2f0162d26d92522d1aa7bcb6b2b3951
Flavian Thuer <flavian.thuer@hotmail.com>
2025-12-07 12:49:56 +0100

    refactor: format train_CUT.sh for improved readability and add execution reminder

--- training/scripts/synthrad25scripts/train_CUT.sh
+++ training/scripts/synthrad25scripts/train_CUT.sh
@@ -1,1 +1,25 @@
-CUDA_VISIBLE_DEVICES=5 python train.py --dataroot '/local/scratch/datasets/FullbodySCT/flavian_subset/7materialized_splits_31baselineBodyRegion/AB/cyclegan/train' --checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints --name cut_THUF --model cut --CUT_mode CUT --input_nc 1 --output_nc 1 --batch_size 200
+CUDA_VISIBLE_DEVICES=5 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/7materialized_splits_31baselineBodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name cut_THUF \
+--model cut \
+--CUT_mode CUT \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 200 \
+--netG unet_256\
+--netD basic
+
+
+##ensure that you execute the script in the traing_cut folder!!! ## 
+CUDA_VISIBLE_DEVICES=7 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/7materialized_splits_31baselineBodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name cut_synthrad_abdomen \
+--model cut \
+--CUT_mode CUT \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 200 \
+--netG unet_256\
+--netD basic
