8113043f6f7248ad91b69220670cb6223d21507b
Flavian Thuer <flavian.thuer@hotmail.com>
2025-12-07 12:29:50 +0100

    feat: update training script for pix2pix with new dataset and parameters

--- training/scripts/synthrad25scripts/train_pix2pix.sh
+++ training/scripts/synthrad25scripts/train_pix2pix.sh
@@ -1,1 +1,9 @@
-CUDA_VISIBLE_DEVICES=6 python train.py --dataroot /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/7materialized_splitsNonNormalizedBodyRegion/AB/pix2pix/AB --checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints --name pix2pix_synthrad_XYZ --model pix2pix --direction AtoB --input_nc 1 --output_nc 1 --batch_size 200
+CUDA_VISIBLE_DEVICES=0 python train.py \
+--dataroot /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/7materialized_splits_31baselineBodyRegion/HN/pix2pix/AB \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name pix2pix_synthrad_head_neck \
+--model pix2pix \
+--direction AtoB \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 96
