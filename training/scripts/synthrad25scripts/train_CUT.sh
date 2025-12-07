d45b5b85dacfb19abcb7cc7cfd63874a675df3eb
Flavian Thuer <flavian.thuer@hotmail.com>
2025-12-07 13:03:13 +0100

    fix: correct script execution folder and ensure proper formatting

--- training/scripts/synthrad25scripts/train_CUT.sh
+++ training/scripts/synthrad25scripts/train_CUT.sh
@@ -12,6 +12,8 @@ CUDA_VISIBLE_DEVICES=5 python train.py \
 
 
 ##ensure that you execute the script in the traing_cut folder!!! ## 
+cd /fullbody-sCT/training_cut/
+
 CUDA_VISIBLE_DEVICES=7 python train.py \
 --dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/7materialized_splits_31baselineBodyRegion/AB/cyclegan/train' \
 --checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
@@ -20,6 +22,4 @@ CUDA_VISIBLE_DEVICES=7 python train.py \
 --CUT_mode CUT \
 --input_nc 1 \
 --output_nc 1 \
---batch_size 200 \
---netG unet_256\
---netD basic
+--batch_size 200 
