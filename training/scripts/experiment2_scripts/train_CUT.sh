2703ee47ebe25dc1c934d02a829f6d344bbf2e12
Flavian Thuer <flavian.thuer@hotmail.com>
2026-02-19 09:09:14 +0100

    Add nyul training configurations for CUT, CycleGAN, and Pix2Pix models

--- training/scripts/experiment2_scripts/train_CUT.sh
+++ training/scripts/experiment2_scripts/train_CUT.sh
@@ -55,3 +55,74 @@ CUDA_VISIBLE_DEVICES=3 python train.py \
 --preprocess None \
 --print_freq 100 \
 --save_epoch_freq 1
+
+
+
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_03LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cut_synthrad_abdomen_34normalized_n4_03LIC \
+--model cut \
+--CUT_mode CUT \
+--output_nc 1 \
+--input_nc 1 \
+--batch_size 1 \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--no_html \
+--preprocess None \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_08LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cut_synthrad_abdomen_34normalized_n4_08LIC \
+--model cut \
+--CUT_mode CUT \
+--output_nc 1 \
+--input_nc 1 \
+--batch_size 1 \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--no_html \
+--preprocess None \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+CUDA_VISIBLE_DEVICES=7 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_centerspecific_03LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cut_synthrad_abdomen_34normalized_n4_centerspecific_03LIC \
+--model cut \
+--CUT_mode CUT \
+--output_nc 1 \
+--input_nc 1 \
+--batch_size 1 \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--no_html \
+--preprocess None \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+CUDA_VISIBLE_DEVICES=6 python train.py \
+--dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_centerspecific_08LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cut_synthrad_abdomen_34normalized_n4_centerspecific_08LIC \
+--model cut \
+--CUT_mode CUT \
+--output_nc 1 \
+--input_nc 1 \
+--batch_size 1 \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--no_html \
+--preprocess None \
+--print_freq 100 \
+--save_epoch_freq 1
--- training/scripts/experiment2_scripts/train_cycleGAN.sh
+++ training/scripts/experiment2_scripts/train_cycleGAN.sh
@@ -49,3 +49,71 @@ CUDA_VISIBLE_DEVICES=3 python train.py --dataroot '/local/scratch/datasets/Fullb
 --n_epochs_decay 0 \
 --print_freq 100 \
 --save_epoch_freq 1
+
+
+
+CUDA_VISIBLE_DEVICES=6 python train.py --dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_03LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cyclegan_abdomen_34normalized_n4_03LIC \
+--model cycle_gan \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 1 \
+--preprocess None \
+--netG unet_256 \
+--netD basic \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+
+CUDA_VISIBLE_DEVICES=5 python train.py --dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_08LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cyclegan_abdomen_34normalized_n4_08LIC \
+--model cycle_gan \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 1 \
+--preprocess None \
+--netG unet_256 \
+--netD basic \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+
+
+CUDA_VISIBLE_DEVICES=5 python train.py --dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_centerspecific_03LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cyclegan_abdomen_34normalized_n4_centerspecific_03LIC \
+--model cycle_gan \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 1 \
+--preprocess None \
+--netG unet_256 \
+--netD basic \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--print_freq 100 \
+--save_epoch_freq 1
+
+
+CUDA_VISIBLE_DEVICES=3 python train.py --dataroot '/local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/experiment2/34normalized_n4_centerspecific_08LIC/7materialized_splits_BodyRegion/AB/cyclegan/train' \
+--checkpoints_dir /local/scratch/datasets/FullbodySCT/Synthrad_combined_preprocessed/8checkpoints \
+--name 2_experiment_cyclegan_abdomen_34normalized_n4_centerspecific_08LIC \
+--model cycle_gan \
+--input_nc 1 \
+--output_nc 1 \
+--batch_size 1 \
+--preprocess None \
+--netG unet_256 \
+--netD basic \
+--n_epochs 100 \
+--n_epochs_decay 0 \
+--print_freq 100 \
+--save_epoch_freq 1
